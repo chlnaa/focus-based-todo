1f6fcb1f0c5eb14b8756989398b09ed5af987bb4
chlnaa <choinaa26@gmail.com>
2026-01-20 20:26:15 +0900

    feat: add routing and setup today page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import { Outlet } from 'react-router';
+import './App.css';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl">focus-todo</h1>
+      <Outlet />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import NotFound from './pages/NotFound.tsx';
+import TodayPage from './pages/TodayPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <TodayPage /> },
+      { path: '/today', element: <TodayPage /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
