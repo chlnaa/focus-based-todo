c88afeb4713bfa7e2bc876cc346a1486f8c65cb7
chlnaa <choinaa26@gmail.com>
2026-01-23 22:02:15 +0900

    feat(focus): add timer logic and control handlers
    - Created formatTimer utility for hh:mm:ss display.
    - Integrated useInterval to manage countdown state.
    - Added handlePreset and handleStopClick for UX consistency.

--- src/pages/FocusPage.tsx
+++ src/pages/FocusPage.tsx
@@ -1,7 +1,48 @@
 import { Button } from '@/components/ui/button';
-import { Play, Square, MoveLeft } from 'lucide-react';
+import { Play, Square, MoveLeft, Pause } from 'lucide-react';
+import { useState } from 'react';
+import { useInterval } from 'usehooks-ts';
+
+type TimerStatus = 'idle' | 'running' | 'paused';
 
 export default function FocusPage() {
+  const [status, setStatus] = useState<TimerStatus>('idle');
+  const [timeLeft, setTimeLeft] = useState(1500);
+  const [initialTime, setInitialTime] = useState(1500);
+
+  const { hours, minutes, seconds } = formatTimer(timeLeft);
+
+  const handlePreset = (minutes: number) => {
+    const seconds = minutes * 60;
+    setTimeLeft(seconds);
+    setInitialTime(seconds);
+    setStatus('idle');
+  };
+
+  const handlePlayClick = () => {
+    if (status === 'running') {
+      setStatus('paused');
+    } else {
+      setStatus('running');
+    }
+  };
+
+  const handleStopClick = () => {
+    setStatus('idle');
+    setTimeLeft(initialTime);
+  };
+
+  useInterval(
+    () => {
+      if (timeLeft > 0) {
+        setTimeLeft((prev) => prev - 1);
+      } else {
+        setStatus('idle');
+      }
+    },
+    status === 'running' ? 1000 : null,
+  );
+
   return (
     <div className="flex flex-col justify-center items-center w-full max-w-175 mt-5 m-auto ">
       <header className="relative w-full text-center">
@@ -13,27 +54,41 @@ export default function FocusPage() {
       <main className="flex flex-col items-center justify-center gap-5 ">
         <div className="flex items-center justify-center text-5xl border-2 rounded-full w-85 h-85 font-mono mt-5 ">
           <div className="flex items-baseline">
-            <span>00</span>
+            <span>{hours}</span>
             <span className="text-sm text-muted-foreground">h</span>
             <span className="text-muted-foreground">:</span>
-            <span>00</span>
+            <span>{minutes}</span>
             <span className="text-sm text-muted-foreground">m</span>
             <span className="text-muted-foreground">:</span>
-            <span>00</span>
+            <span>{seconds}</span>
             <span className="text-sm text-muted-foreground">s</span>
           </div>
         </div>
 
         <div className="flex gap-3 p-2">
-          <Button variant={'outline'}>00:25</Button>
-          <Button variant={'outline'}>00:50</Button>
+          <Button
+            variant={initialTime === 1500 ? 'default' : 'outline'}
+            onClick={() => handlePreset(25)}
+          >
+            00:25
+          </Button>
+          <Button
+            variant={initialTime === 3000 ? 'default' : 'outline'}
+            onClick={() => handlePreset(50)}
+          >
+            00:50
+          </Button>
           <Button variant={'outline'}>Custom</Button>
         </div>
         <div className="flex gap-6 items-center">
-          <Button className="rounded-full w-15 h-15">
-            <Play />
+          <Button className="rounded-full w-15 h-15" onClick={handlePlayClick}>
+            {status === 'running' ? <Pause /> : <Play />}
           </Button>
-          <Button variant={'outline'} className="rounded-full w-15 h-15">
+          <Button
+            className="rounded-full w-15 h-15"
+            variant={'outline'}
+            onClick={handleStopClick}
+          >
             <Square />
           </Button>
         </div>
@@ -50,4 +105,14 @@ export default function FocusPage() {
       </footer>
     </div>
   );
+}
+
+function formatTimer(setTime: number) {
+  const pad = (n: number) => n.toString().padStart(2, '0');
+
+  const hours = pad(Math.floor(setTime / 3600));
+  const minutes = pad(Math.floor((setTime % 3600) / 60));
+  const seconds = pad(setTime % 60);
+
+  return { hours, minutes, seconds };
 }
