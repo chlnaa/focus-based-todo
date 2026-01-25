861b6a5c38700b4378286c340d4b8705bbc6cf6b
chlnaa <choinaa26@gmail.com>
2026-01-25 20:55:44 +0900

    refactor: Style completed todos on Today page

--- src/features/todo/TodoItems.tsx
+++ src/features/todo/TodoItems.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import { formatTime } from '@/lib/utils';
+import { cn, formatTime } from '@/lib/utils';
 import { useToggleTodo } from '@/stores/useTodoStore';
 import type { Todo } from '@/types/types';
 import { Pencil, Trash2 } from 'lucide-react';
@@ -47,7 +47,10 @@ export default function TodoItems({ todo, onDelete, onUpdate }: TodoItemProps) {
   return (
     <li
       key={todo.id}
-      className="flex flex-col justify-between items-center p-2 border-2 rounded-xl gap-4 mb-5"
+      className={cn(
+        'flex flex-col justify-between items-center p-2 border-2 rounded-xl gap-4 mb-5',
+        todo.status === 'completed' && 'opacity-60 grayscale[0.5]',
+      )}
     >
       <div className="flex justify-between items-center w-full">
         <div className="flex items-center flex-1 gap-2">
@@ -65,16 +68,31 @@ export default function TodoItems({ todo, onDelete, onUpdate }: TodoItemProps) {
               autoFocus
             />
           ) : (
-            <label className="p-2 " id={id}>
+            <label
+              className={cn(
+                'p-2 text-xl',
+                todo.status === 'completed' &&
+                  'line-through text-muted-foreground',
+              )}
+              id={id}
+            >
               {text}
             </label>
           )}
         </div>
         <div>
-          <Button variant={'ghost'} onClick={handleEditClick}>
+          <Button
+            variant={'ghost'}
+            onClick={handleEditClick}
+            disabled={todo.status === 'completed'}
+          >
             <Pencil />
           </Button>
-          <Button variant={'ghost'} onClick={handleDelete}>
+          <Button
+            variant={'ghost'}
+            onClick={handleDelete}
+            disabled={todo.status === 'completed'}
+          >
             <Trash2 />
           </Button>
         </div>
@@ -83,7 +101,12 @@ export default function TodoItems({ todo, onDelete, onUpdate }: TodoItemProps) {
         <div className="mr-5">
           {formatTime(totalFocusTime || 0).fullTimeDisplay}
         </div>
-        <Button onClick={() => navigate(`/focus/${id}`)}>startFocus</Button>
+        <Button
+          onClick={() => navigate(`/focus/${id}`)}
+          disabled={todo.status === 'completed'}
+        >
+          startFocus
+        </Button>
       </div>
     </li>
   );
