a3a187cf33a8b4bf24dc86f9c329077b3d6d03e5
chlnaa <choinaa26@gmail.com>
2026-01-27 17:15:47 +0900

    feat(Navbar): implement Navbar with new brand name 'FocusDo'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,21 @@
+import { AlarmClockCheck, Sun, Menu } from 'lucide-react';
+import { Link } from 'react-router';
+
 export default function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <nav className="flex justify-between items-center">
+      <Link to={'/'}>
+        <div className="flex items-center gap-2">
+          <AlarmClockCheck />
+          <h1 className="text-2xl font-bold">FocusDo</h1>
+        </div>
+      </Link>
+      <div className="flex items-center gap-5">
+        <Sun />
+        <Link to={'/history'}>
+          <Menu />
+        </Link>
+      </div>
+    </nav>
+  );
 }
