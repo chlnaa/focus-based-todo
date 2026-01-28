b1fbc53e006f89e02fabf45ce48dce318f690c5f
chlnaa <choinaa26@gmail.com>
2026-01-28 19:36:56 +0900

    refactor: move navbar to components/header directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/header/Navbar';
 
 function App() {
   return (
