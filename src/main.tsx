b7a778c3ee35b8d06ce50274c9440dc4c245641c
chlnaa <choinaa26@gmail.com>
2026-02-04 23:53:49 +0900

    chore: setup TanStack Query and shadcn skeleton

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import TodayPage from './pages/TodayPage.tsx';
 import FocusPage from './pages/FocusPage.tsx';
 import FocusHistoryPage from './pages/FocusHistoryPage.tsx';
 import { Toaster } from './components/ui/sonner.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -27,6 +30,8 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster richColors position="top-center" />
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
