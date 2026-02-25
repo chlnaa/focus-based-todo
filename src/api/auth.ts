ebe0f715c2cbd903266e6fcbb42c99ec5c8e4eb4
chlnaa <choinaa26@gmail.com>
2026-02-25 19:53:47 +0900

    feat(auth): add auth utility functions
    - add signOut utility
    - add requestPasswordResetEmail utility
    - add updatePassword utility

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -24,3 +24,31 @@ export async function signInWithPassword({ email, password }: AuthParams) {
   if (error) throw error;
   return data;
 }
+
+export async function signOut() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    await supabase.auth.signOut({
+      scope: 'local',
+    });
+  }
+}
+
+export async function requestPasswordResetEmail(email: AuthParams['email']) {
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: `${import.meta.env.VITE_PUBLIC_URL}/reset-password`,
+  });
+
+  if (error) throw error;
+  return data;
+}
+
+export async function updatePassword(password: AuthParams['password']) {
+  const { data, error } = await supabase.auth.updateUser({
+    password,
+  });
+
+  if (error) throw error;
+  return data;
+}
