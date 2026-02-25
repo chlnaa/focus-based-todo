29b1fbfa0f8fe369a3d68acdf31f76c51abb2d1b
chlnaa <choinaa26@gmail.com>
2026-02-25 20:08:53 +0900

    refactor(ui): update navbar based on auth state
    - consume session in Navbar
    - refactor MenuButton from Popover to DropdownMenu

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,20 +1,23 @@
 import { AlarmClockCheck, Sun } from 'lucide-react';
 import { Link } from 'react-router';
 import MenuButton from './MenuButton';
+import { useSession } from '@/stores/session';
 
 export default function Navbar() {
+  const session = useSession();
+
   return (
-    <nav className="flex justify-between items-center">
+    <header className="flex justify-between items-center">
       <Link to={'/'}>
         <div className="flex items-center gap-2">
           <AlarmClockCheck className="w-5 h-5 sm:w-6 sm:h-6" />
           <h1 className="text-lg sm:text-2xl font-bold">FocusDo</h1>
         </div>
       </Link>
-      <div className="flex items-center gap-5">
+      <nav className="flex items-center gap-5">
         <Sun className="w-5 h-5 sm:w-6 sm:h-6" />
-        <MenuButton />
-      </div>
-    </nav>
+        {session && <MenuButton />}
+      </nav>
+    </header>
   );
 }
