39fe6496437a9b820366bf56b3b7158c3a7ad026
chlnaa <choinaa26@gmail.com>
2026-04-04 22:03:02 +0900

    refactor: remove onChangeTheme handler from ThemeBtn

--- src/components/header/ThemeBtn.tsx
+++ src/components/header/ThemeBtn.tsx
@@ -6,24 +6,12 @@ import {
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
 import type { Theme } from '@/types/types';
+import { useSetTheme } from '@/stores/themeStore';
 
 const THEMES: Theme[] = ['system', 'dark', 'light'];
 
 export default function ThemeBtn() {
-  const onChangeTheme = (theme: Theme) => {
-    const htmlTag = document.documentElement;
-    htmlTag.classList.remove('dark', 'light');
-
-    if (theme === 'system') {
-      const isDarkTheme = window.matchMedia(
-        '(prefers-color-scheme:dark)',
-      ).matches;
-
-      htmlTag.classList.add(isDarkTheme ? 'dark' : 'light');
-    } else {
-      htmlTag.classList.add(theme);
-    }
-  };
+  const setTheme = useSetTheme();
 
   return (
     <DropdownMenu>
@@ -35,7 +23,7 @@ export default function ThemeBtn() {
           <DropdownMenuItem key={`theme-button-${theme}`} asChild>
             <div
               className="hover:bg-muted cursor-pointer p-3"
-              onClick={() => onChangeTheme(theme)}
+              onClick={() => setTheme(theme)}
             >
               {theme}
             </div>
