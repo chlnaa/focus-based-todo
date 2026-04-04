2630ae655135d07d7ee3d40fc8c83b50f1360afc
chlnaa <choinaa26@gmail.com>
2026-04-04 22:56:56 +0900

    refactor: integrate devtools, persist, and combine into ThemeStore and update hooks

--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -1,34 +1,49 @@
 import type { Theme } from '@/types/types';
 import { create } from 'zustand';
+import { combine, devtools, persist } from 'zustand/middleware';
 
-type ThemeStore = {
+type State = {
   theme: Theme;
-  setTheme: (theme: Theme) => void;
 };
 
-const applyTheme = (theme: Theme) => {
-  const htmlTag = document.documentElement;
-  htmlTag.classList.remove('dark', 'light');
-
-  if (theme === 'system') {
-    const isDarkTheme = window.matchMedia(
-      '(prefers-color-scheme:dark)',
-    ).matches;
-
-    htmlTag.classList.add(isDarkTheme ? 'dark' : 'light');
-  } else {
-    htmlTag.classList.add(theme);
-  }
+const initialState: State = {
+  theme: 'light',
 };
 
-const useThemeStore = create<ThemeStore>((set) => ({
-  theme: 'system',
+const useThemeStore = create(
+  devtools(
+    persist(
+      combine(initialState, (set) => ({
+        actions: {
+          setTheme: (theme: Theme) => {
+            const htmlTag = document.documentElement;
+            htmlTag.classList.remove('dark', 'light');
 
-  setTheme: (theme) => {
-    localStorage.setItem('theme', theme);
-    applyTheme(theme);
-    set({ theme });
-  },
-}));
+            if (theme === 'system') {
+              const isDarkTheme = window.matchMedia(
+                '(prefers-color-scheme:dark)',
+              ).matches;
 
-export const useSetTheme = () => useThemeStore((store) => store.setTheme);
+              htmlTag.classList.add(isDarkTheme ? 'dark' : 'light');
+            } else {
+              htmlTag.classList.add(theme);
+            }
+            set({ theme });
+          },
+        },
+      })),
+      {
+        name: 'ThemeStore',
+        partialize: (store) => ({
+          theme: store.theme,
+        }),
+      },
+    ),
+    { name: 'ThemeStore' },
+  ),
+);
+
+export const useTheme = () => useThemeStore((store) => store.theme);
+
+export const useSetTheme = () =>
+  useThemeStore((store) => store.actions.setTheme);
